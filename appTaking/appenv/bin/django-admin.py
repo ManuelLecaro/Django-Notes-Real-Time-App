a1053e428a8e281ea4de02f22c7a2dc039bda576
Manuel Lecaro <mnlecaro@gmail.com>
2020-03-13 18:42:42 -0500

    adding requirements and env

---
+++ appTaking/appenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/manuel/Documents/Django-Notes-Real-Time-App/appTaking/appenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
